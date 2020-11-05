269d077e2c45ebbe832160f0715b317b46adb581
Andrew Vogan <avogan@artandlogic.com>
2020-11-05 16:52:54 -0500

    Initial commit.

---
+++ generate.py
@@ -0,0 +1,64 @@
+import os
+import os.path
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+NAMES = ['biden', 'trump']
+COLORS = ['blue', 'red']
+LEADINGDIGITS = list(range(10)[1:])
+
+SRCDATA = "source_data"
+README = 'readme.txt'
+
+def getDataset(name, basePath):
+   filePath = os.path.join(basePath, name + ".txt")
+   votes = [int(i) for i in open(filePath).read().strip().split('\n')]
+   return votes
+
+def getLeadingDigits(votes):
+   return [sum([1 for i in votes if str(i).startswith(str(leadingDigit))])
+      for leadingDigit in LEADINGDIGITS]
+
+def getBenfordsLawSample(numItems):
+   standardBenfordsLawPercents = [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
+   return [int(p * numItems / 100) for p in standardBenfordsLawPercents]
+
+with open('readme.md', 'w') as f:
+   for dataDirName in next(os.walk(SRCDATA))[1]:
+      basePath = os.path.join(SRCDATA, dataDirName)
+      readMePath = os.path.join(basePath, README)
+      if not (os.path.isfile(readMePath) and \
+            all([os.path.isfile(os.path.join(SRCDATA, dataDirName, f"{name}.txt")) for name in NAMES])):
+         print(f"Didn't find expected data in {dataDirName}")
+         continue
+
+      print(f"Processing {dataDirName}...")
+
+      fig = plt.figure()
+      for nameIdx in range(len(NAMES)):
+         name = NAMES[nameIdx]
+
+         N = len(LEADINGDIGITS)
+         width = 0.3
+         plot = fig.add_subplot(121 + nameIdx)
+         y_pos = np.arange(N)
+
+         votes = getDataset(name, basePath)
+
+         seriesValsBenford = getBenfordsLawSample(len(votes))
+         plot.bar(y_pos - width/2, seriesValsBenford, width, color='gray')
+         plot.xaxis.set_ticks([i - 1 for i in LEADINGDIGITS])
+         plot.set_xticklabels([str(x) for x in LEADINGDIGITS])
+
+         seriesVals = getLeadingDigits(votes)
+         plot.bar(y_pos + width/2, seriesVals, width, color=COLORS[nameIdx])
+      
+      imagePath = os.path.join(basePath, f"{dataDirName}.png")
+      plt.savefig(imagePath, dpi=150)
+
+      f.write('***\n')
+      f.write(f'#`{dataDirName}`#\n')
+      f.write(open(readMePath).read())
+      f.write('\n')
+      f.write(f'![alt text]({imagePath} "Title"))\n')
---
+++ source_data/milwaukee_wards_pres_2020/readme.txt
@@ -0,0 +1,5 @@
+Data extracted from the following page on 2020-11-05 at around 1530 Eastern Time.
+
+https://county.milwaukee.gov/EN/County-Clerk/Off-Nav/Election-Results/Election-Results-Fall-2020
+
+Dataset is Biden and Trump 2020 Pres/VP General Election vote totals for wards in Milwaukee, WI.
